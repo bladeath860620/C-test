314859e7381a47b3de5dc2444b0e8c8a73ece5de
skywater860620 <bladeath860620@gmail.com>
2016-02-05 13:55:04 +0800

    ".h" file practice

---
+++ test5.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "test5.h"
+using namespace std;
+
+int main()
+{
+	int a = 2;
+	cout << t3(a) << endl;
+} 
---
+++ test5.h
@@ -0,0 +1,9 @@
+#ifndef times_three
+#define times_three
+
+int t3(int x)
+{
+	x = x *x *x;
+	return x;
+}
+#endif
